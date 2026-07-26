2c872edb3120e262b1e26fc2016fd9e1762ac57c
intwitch <bloxrocks0789@gmail.com>
2026-07-26 01:34:32 -0400

    Add Curses

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,24 +2,35 @@ var xhr = new XMLHttpRequest();
 xhr.open("GET", "data/OmniGachaItemList.csv", false);
 xhr.send(null);
 
-var csvDataRaw = CSVToArray(xhr.responseText);
+var itemsCSVDataRaw = CSVToArray(xhr.responseText);
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "data/OmniGachaCurses.csv", false);
+xhr.send(null);
+
+var cursesCSVDataRaw = CSVToArray(xhr.responseText);
 
 var NSFW = false;
 var NSFWOnly = false;
 
-var currentRoll;
-//big string of CSV
-var savedRolls = [];
+var currentItemRoll;
+var savedItemRolls = [];
+
+var currentCurseRoll;
+var savedCurseRolls = [];
+
+var itemsTab;
+var cursesTab;
 
 //designed to be repeatidly callled for multiple filters in search. returns a new array with the filtered data.
 function filterCSVData(csvData, filter) {
 	return csvData.filter((item) => filter(item));
 }
 
-function NSFWfilter(NSFW, NSFWOnly) {
-	if(NSFW == false) return filterCSVData(csvDataRaw, function(item) {return item[20] == "FALSE"});
-	if(NSFWOnly == true) return filterCSVData(csvDataRaw, function(item) {return item[20] == "TRUE"});
-	return csvDataRaw;
+function NSFWfilter(Data, NSFW, NSFWOnly, index) {
+	if (NSFW == false) return filterCSVData(Data, function (item) { return item[index] == "FALSE" });
+	if (NSFWOnly == true) return filterCSVData(Data, function (item) { return item[index] == "TRUE" });
+	return Data;
 }
 
 
@@ -28,63 +39,52 @@ function getRandomItem(itemArray) {
 	return itemArray[randomIndex];
 }
 
-function roll() {
-	var newRow = document.createElement("tr");
-	var element = getRandomItem(ticketData);
-	currentRoll = element;
-	newRow.innerHTML = "<td>" + element[0] + "</td><td>" + element[1] + "</td><td>" + element[2] + "</td><td>" + element[3] + "</td><td>" + element[4] + "</td><td>" + element[5] + "</td><td>" + element[6] + "</td><td>" + element[7] + "</td><td>" + element[8] + "</td><td>" + element[9] + "</td><td>" + element[10] + "</td><td>" + element[11] + "</td><td>" + element[12] + "</td><td>" + element[13] + "</td><td>" + element[14] + "</td><td>" + element[15] + "</td><td>" + element[16] + "</td><td>" + element[17] + "</td><td>" + element[18] + "</td><td>" + element[19] + "</td><td>" + element[20] + "</td>";
+function roll(table, data) {
+	var element = getRandomItem(data);
+	var newRow = createRow(element);
 
-	document.querySelector("#rollHistory tr:first-child").after(document.querySelector("#rollTable tbody tr:last-child"))
-	document.querySelector("#rollTable tbody").append(newRow);
+	table.querySelector("tbody tr").replaceWith(newRow);
+
+	return element;
 }
 
-function save() {
-	const saveTable = document.getElementById("saveTable");
-	savedRolls.push(currentRoll);
-	newRow = createRow(currentRoll);
-	var deleteItem = document.createElement("td")
-	var deleteButton = document.createElement("button")
-	deleteButton.innerText = "Remove";
+function redrawSaveTable(table, data) {
+	var rows = [];
+	data.forEach(function (item, index) {
+		var row = createRow(item)
+		var deleteItem = document.createElement("td");
+		var deleteButton = document.createElement("button");
+		deleteButton.innerText = "Remove"
+		deleteItem.appendChild(deleteButton);
 
-	deleteItem.appendChild(deleteButton);
-	newRow.appendChild(deleteItem);
-	deleteButton.addEventListener("click", function(){
+		deleteButton.addEventListener("click", function () {
+			data.splice(index, 1);
+			redrawSaveTable(table, data);
+		});
 
-		/*okay, this is weird and complicated.
-		if we pass index of current item in array within two deletes they could get desnycronized
-		index delete buttons will end up looking like:
-		[1, 2, 3, 4] -> [1, 2, 4]
-		so instead, my approach is to figure out how many previous siblings the row element has, and pass that to the splice.
-		previous element loops until null then returns count.
+		row.appendChild(deleteItem);
+		rows.push(row);
+	})
 
-		a better solution may be a map or something but......... idk.
-		*/
-
-		var index = -1; //it's somehow managing to find a previous sibling. I don't get it either.
-		var row = deleteButton.parentElement.parentElement;
-		while (row.previousSibling != null){
-			index++;
-			row = row.previousSibling;
-		}
-
-		savedRolls.splice(index, 1);
-		deleteButton.parentElement.parentElement.remove(); //scary
-	});
-
-	saveTable.getElementsByTagName("tbody")[0].append(newRow);
+	drawTableBody(table, rows);
 }
 //given array convert to tr element with td data
 //TODO: Implement everywhere
-function createRow(array){
+function createRow(array) {
 	var newRow = document.createElement("tr");
-	newRow.innerHTML = "<td>" + array[0] + "</td><td>" + array[1] + "</td><td>" + array[2] + "</td><td>" + array[3] + "</td><td>" + array[4] + "</td><td>" + array[5] + "</td><td>" + array[6] + "</td><td>" + array[7] + "</td><td>" + array[8] + "</td><td>" + array[9] + "</td><td>" + array[10] + "</td><td>" + array[11] + "</td><td>" + array[12] + "</td><td>" + array[13] + "</td><td>" + array[14] + "</td><td>" + array[15] + "</td><td>" + array[16] + "</td><td>" + array[17] + "</td><td>" + array[18] + "</td><td>" + array[19] + "</td><td>" + array[20] + "</td>";
+	var rowData = "";
+	for (var i = 0; i < array.length; i++) {
+		rowData += "<td>" + array[i] + "</td>";
+	}
+	newRow.innerHTML = rowData;
 	return newRow;
 }
 //given a table and an array of rows (tr element) , clear then draw them to tbody
-function drawTableBody(table, rows){
+function drawTableBody(table, rows) {
 	var tbody = table.getElementsByTagName("tbody")[0];
-	rows.forEach(function(row) {
-		tbody.append.Child(row)
+	tbody.innerHTML = "";
+	rows.forEach(function (row) {
+		tbody.appendChild(row)
 	});
 }
 
@@ -94,7 +94,7 @@ function updateTicketData() {
 	switch (ticketValue) {
 		case "0":
 		default:
-			ticketData = csvData;
+			ticketData = itemsCSVData;
 			return;
 		case "1":
 			filter = ["F", "E", "D"];
@@ -122,18 +122,18 @@ function updateTicketData() {
 			break;
 	}
 
-	ticketData = filterCSVData(csvData, function (item) { return filter.includes(item[14]) });
+	ticketData = filterCSVData(itemsCSVData, function (item) { return filter.includes(item[14]) });
 }
 
 function searchFor(string) {
 	var regex = new RegExp(string, "i");
-	var searchResults = csvData.filter(function(item) {
+	var searchResults = itemsCSVData.filter(function (item) {
 		return item[0].concat(item[1], item[2]).search(regex) != -1;
 	});
 
 	var tbody = document.querySelector("#searchTable > tbody");
-	tbody.innerHTML 
-	= ""; //wipe before replace
+	tbody.innerHTML
+		= ""; //wipe before replace
 
 	searchResults.forEach(element => {
 		var newRow = document.createElement("tr");
@@ -142,44 +142,96 @@ function searchFor(string) {
 	});
 }
 
-function exportSaved(){
+function exportSavedItems() {
 	var link = document.createElement("a");
-	var file = new Blob([savedRolls], {type: 'text/plan'});
+	var file = new Blob([savedItemRolls], { type: 'text/plan' });
 	link.href = URL.createObjectURL(file)
 	link.download = "Omni Gacha rolls.csv"
 	link.click();
 	URL.revokeObjectURL(link.href);
-	console.log(savedRolls);
+	console.log(savedItemRolls);
 }
 
-window.onload = function() {
-	document.getElementById("contentOptions").addEventListener("change", function() {
+function exportSavedCurses() {
+	var link = document.createElement("a");
+	var file = new Blob([savedCurseRolls], { type: 'text/plan' });
+	link.href = URL.createObjectURL(file)
+	link.download = "Omni Gacha curses.csv"
+	link.click();
+	URL.revokeObjectURL(link.href);
+	console.log(savedCurseRolls);
+}
+
+function hideAllBut(tab) {
+	cursesTab.style.display = "none";
+	itemsTab.style.display = "none";
+	tab.style.display = "block";
+}
+
+window.onload = function () {
+	document.getElementById("contentOptions").addEventListener("change", function () {
 		NSFW = document.getElementById("nsfwCheckbox").checked;
 		NSFWOnly = document.getElementById("nsfwOnlyCheckbox").checked;
 		if (NSFW) document.querySelector("#contentOptions > span:last-child").style.visibility = "visible";
 		else document.querySelector("#contentOptions > span:last-child").style.visibility = "hidden";
 
-		csvData = NSFWfilter(NSFW, NSFWOnly);
+		itemsCSVData = NSFWfilter(itemsCSVDataRaw, NSFW, NSFWOnly, itemsCSVData.length - 1);
 		updateTicketData();
 	});
 
 	document.getElementById("ticketSelector").addEventListener("change", updateTicketData);
-	/*
-	var searchfield = document.querySelector("#search > div > input");
-	searchfield.addEventListener("onkeyup", searchFor(searchfield.value));
-	no, I don't know why this didn't work.
-	*/
+
+	this.itemsTab = document.getElementById("items");
+	this.cursesTab = document.getElementById("curses");
+
+
+	document.getElementById("itemsButton").addEventListener("click", function () {
+		hideAllBut(itemsTab);
+	});
+	document.getElementById("cursesButton").addEventListener("click", function () {
+		hideAllBut(cursesTab);
+	});
+	document.getElementById("rollButton").addEventListener("click", function () {
+		currentItemRoll = roll(document.getElementById("rollTable"), ticketData);
+	});
+	document.getElementById("saveButton").addEventListener("click", function () {
+		savedItemRolls.push(currentItemRoll);
+		redrawSaveTable(document.getElementById("saveTable"), savedItemRolls)
+	});
+
+	document.getElementById("cursesRollButton").addEventListener("click", function () {
+		currentCurseRoll = roll(document.getElementById("cursesRollTable"), cursesCSVData);
+	});
+	document.getElementById("cursesSaveButton").addEventListener("click", function () {
+		savedCurseRolls.push(currentCurseRoll);
+		redrawSaveTable(document.getElementById("cursesSaveTable"), savedCurseRolls)
+	});
+
+	document.getElementById("historyButton").addEventListener("click", function () {
+		document.getElementById("rollHistory").style.display = "block";
+	});
+	document.getElementById("searchOpenButton").addEventListener("click", function () {
+		document.getElementById("search").style.display = "block";
+	});
+	document.getElementById("searchModalButton").addEventListener("click", function () {
+		searchFor(document.querySelector("#search > div > input").value);
+	});
+	document.getElementById("exportButton").addEventListener("click", exportSavedItems);
+	document.getElementById("cursesExport").addEventListener("click", exportSavedCurses);
+
+
+	hideAllBut(itemsTab);
 };
 
-window.onclick = function (event) {
+window.addEventListener("click", function (event) {
 	var historyModal = document.getElementById("rollHistory");
 	var searchModal = document.getElementById("search");
-	if (event.target == historyModal || event.target == searchModal) {
-		historyModal.style.display = 'none';
-		searchModal.style.display = 'none';
+	if (event.target === historyModal || event.target === searchModal) {
+		historyModal.style.display = "none";
+		searchModal.style.display = "none";
 	}
-} 
+});
 
-var csvData = NSFWfilter(NSFW, NSFWOnly);
-
-var ticketData = csvData; //default
+var itemsCSVData = NSFWfilter(itemsCSVDataRaw, NSFW, NSFWOnly, itemsCSVDataRaw[0].length - 1);
+var cursesCSVData = NSFWfilter(cursesCSVDataRaw, NSFW, NSFWOnly, 6);
+var ticketData = itemsCSVData; //default
