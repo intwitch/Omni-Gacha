b613e5ae154e74987ce075558537c7239c7e3249
intwitch <bloxrocks0789@gmail.com>
2026-08-01 18:21:42 -0400

    finish and rename CSVbuild to dataBuild prep for conversion of data parsing from csv to json, superior format in use case.

---
+++ dataBuild.js
@@ -0,0 +1,60 @@
+require('dotenv').config({ path: '.env'});
+require('url').URL
+
+const fs = require('node:fs')
+const https = require('https')
+
+//setup get request
+const spreadsheetID = "1YxfI5xpi_q5IEi0L2-3fW_GzCba1SnpS_nOzWVIBwO4"
+const ranges = ["\'Item List\'!A1:V2"
+    ,"\'Curses\'!A1:H2"]
+
+const url = encodeURI(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetID}/values:batchGet?key=${process.env.SHEETSAPIKEY}&ranges=${ranges[0]}&ranges=${ranges[1]}`)
+var result = ""
+
+//define functions
+
+// function for filter to call
+function filterData(value, index, array) {
+    if(value[0] === "" || value[1] === "" || value[2] === ""){
+        return false;
+    }
+    else return true;
+}
+
+// with json gotten, parse, filter, create, and save new json file.
+function buildData() {
+    result = JSON.parse(result);
+
+    var itemsData = result.valueRanges[0].values.filter(filterData);
+    var cursesData = result.valueRanges[1].values.filter(filterData);
+
+    var finalJson = JSON.stringify({
+        items: itemsData,
+        curses: cursesData
+    });
+    //overwrite is desireable. Files shouldn't be tracked as they will be built when the site deploys.
+    fs.writeFile('docs/data/values.json', finalJson, callback)
+}
+
+//minor error handling in file write
+function callback(err){
+    if (err){ 
+        console.log(err)
+        //finish anything left, exit gracefully with error code.
+        process.exitCode = 1
+    }
+}
+
+//get data from sheet
+https.get({
+    hostname: 'sheets.googleapis.com',
+    path: url
+}, (res) => {
+
+    res.on('data', (chunk) => {
+      result += chunk;
+    });
+
+    res.on('end', buildData);
+});
