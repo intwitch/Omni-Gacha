ed36f9e94b1cea4095c9f1300241d98f6c676fad
intwitch <bloxrocks0789@gmail.com>
2026-08-01 19:48:46 -0400

    rewrite code to use JSON

--- dataBuild.js
+++ dataBuild.js
@@ -6,8 +6,8 @@ const https = require('https')
 
 //setup get request
 const spreadsheetID = "1YxfI5xpi_q5IEi0L2-3fW_GzCba1SnpS_nOzWVIBwO4"
-const ranges = ["\'Item List\'!A1:V2"
-    ,"\'Curses\'!A1:H2"]
+const ranges = ["\'Item List\'!A:U"
+    ,"\'Curses\'!A:H"]
 
 const url = encodeURI(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetID}/values:batchGet?key=${process.env.SHEETSAPIKEY}&ranges=${ranges[0]}&ranges=${ranges[1]}`)
 var result = ""
@@ -25,9 +25,12 @@ function filterData(value, index, array) {
 // with json gotten, parse, filter, create, and save new json file.
 function buildData() {
     result = JSON.parse(result);
+    console.log("Finished API Request")
 
     var itemsData = result.valueRanges[0].values.filter(filterData);
     var cursesData = result.valueRanges[1].values.filter(filterData);
+
+    console.log(`final data obtained.\nItems: ${itemsData.length}\nCurses: ${cursesData.length}`)
 
     var finalJson = JSON.stringify({
         items: itemsData,
@@ -47,13 +50,14 @@ function callback(err){
 }
 
 //get data from sheet
+console.log("Quereying API")
 https.get({
     hostname: 'sheets.googleapis.com',
     path: url
 }, (res) => {
 
     res.on('data', (chunk) => {
-      result += chunk;
+        result += chunk;
     });
 
     res.on('end', buildData);
