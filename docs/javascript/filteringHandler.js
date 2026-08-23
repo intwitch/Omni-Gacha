c41283ab64a7b8d7fdab3dc408e57cd7ccbcf3c6
bloxrocks <bloxrocks0789@gmail.com>
2026-08-23 14:19:39 -0400

    function setup and inital function rewrites

--- docs/javascript/filteringHandler.js
+++ docs/javascript/filteringHandler.js
@@ -1,195 +1,207 @@
 import {
 	CURSES,
 	ITEMS,
-	NSFW,
-	NSFWOnly,
-	cursesData,
-	itemsData,
-	optionsValues,
-	rawCursesData,
-	rawItemsData,
 } from "./main.js";
-import { optionsCookieSetFunction } from "./cookies.js";
+import {
+	gachaBuildsOptionsHandler
+}
+	from "./gachaBuildsOptionsHandler.js"
 
-function contentFilterChange(){
-	NSFWCheckBox = document.getElementById("nsfwCheckbox")
-	NSFWOnlyCheckBox = document.getElementById("nsfwOnlyCheckbox")
-
-	NSFW = NSFWCheckBox.checked
-	NSFWOnly = NSFWOnlyCheckBox.checked
-
-	//TODO refactor out old variables outside options.
-	optionsValues.NSFW = NSFW
-	optionsValues.NSFWOnly = NSFWOnly
-
-	optionsCookieSetFunction();
-
-	var searchParam
-
-	if (NSFW == true) {
-		if (NSFWOnly == false) {
-			// NSFW = true and NSFWOnly = false
-			itemsData = rawItemsData;
-			cursesData = rawCursesData;
-			NSFWOnlyCheckBox.parentElement.style.visibility = "visible"
-			updateFilterData();
-			return;
-		} else {
-			// NSFWONLY && NSFW
-			NSFWOnlyCheckBox.parentElement.style.visibility = "visible"
-			searchParam = true
-		}
-	} else {
-		// NSFW = false and thus NSFWONLY = false
-		searchParam = false
-		NSFWOnlyCheckBox.checked = false
-		NSFWOnlyCheckBox.parentElement.style.visibility = "hidden"
-	}
-	updateContentFilter(searchParam)
+export {
+	filteringHandler
 }
 
 /**
- * refilter ItemsData and CursesData based on boolean input
- * if you want some NSFW but not only this function shoulld not be called
- * skip straight to updateFilterData() instead
- * @param {boolean} searchParam 
+ * handle all filtering, prepping, 
+ * and searching (which is also just filtering to user defined paramaters)
+ * do not read/write to dom do not pass go, do not collect 100 dollars, etc.
+ * do keep track of filters, and change when function called
  */
-function updateContentFilter(searchParam) {
+class filteringHandler {
+	//set here to get intellesense to pick up on type, will always be set by constructor anyway
+	#buildsOptions = gachaBuildsOptionsHandler()
+	#data = {
+		raw: {
+			items: [],
+			curses: []
+		},
 
-	var filterFunction = function (seekPosition) {
-		var rFunction = function (value, index, array) {
-			return value[seekPosition] === searchParam.toString().toUpperCase()
+		filtered: {
+			items: [],
+			curses: []
+		},
+
+		roll: {
+			items: [],
+			curses: []
 		}
-		return rFunction
 	}
 
-	itemsData = rawItemsData.filter(filterFunction(ITEMS.NSFW))
-	cursesData = rawCursesData.filter(filterFunction(CURSES.NSFW))
+	#filters = {
+		ranks: [
+			"F", "E", "D", "C", "B", "A", "S", "SS", "SSS", "EX"
+		],
+		categories: [
+			"Ability", "Arsenal", "Consumable", "Familiar", "Farmable", "Novelty", "Passive", "Race", "Skill", "Vehicle", "Wearible", "World"
+		]
+	}
 
-	updateFilterData();
-}
+	/**
+	 * 
+	 * @param {gachaBuildsOptionsHandler} gachaBuildsOptionsHandler 
+	 * @param {object} data
+	 */
+	constructor(gachaBuildsOptionsHandler, data) {
+		this.#buildsOptions = gachaBuildsOptionsHandler
+		this.#data = data
+	}
 
-//calls all filter related functions for items as it updates the global variable
-function updateItemFilterData() {
-	filteredItemsData = itemsData;
-	filteredItemsData = FilterTicketData(filteredItemsData);
-	filteredItemsData = filterItemByCategory(filteredItemsData);
-	//console.log(filteredItemsData);
-}
 
-function updateCurseFilterData(){
-	filteredCursesData = cursesData;
-}
+	updateContentFilter() {
+		const nsfw = this.#buildsOptions.getOption("NSFW")
+		const nsfwOnly = this.#buildsOptions.getOption("NSFWOnly")
 
-function updateFilterData(){
-	updateItemFilterData()
-	updateCurseFilterData()
-}
+		let searchParam
+		let doFilter = true;
 
-// create a function to filter on based on an array of filters and an index to see if the index of the item/curse is in the filter.
-function valueFilter(filterArray, index) {
-	var filterFunction = function (value) {
-		const valuePart = value[index].toLowerCase();
-		for (filter of filterArray) {
-			if (valuePart === filter.toLowerCase()) return true
+		if(nsfw){
+			if(nsfwOnly){
+				searchParam = "TRUE"
+			}
+			else doFilter = false;
 		}
-		return false;
-	}
-	return filterFunction
-}
-//same as above, but filter function looks for text matching, not exact matching. Unless it's a rank, where it has to be valueFiltered
-function textValueFilter(filterArray, index) {
-	var filterFunction = function (value) {
-		const valuePart = value[index].toLowerCase();
-		for (filter of filterArray) {
-			if (valuePart.includes(filter.toLowerCase())) return true
+		else searchParam = "FALSE"
+
+		var filterFunctionCreator = function (seekPosition) {
+			var filterFunction = function (value, index, array) {
+				return value[seekPosition] == searchParam
+			}
+			return filterFunction
 		}
-		return false;
-	}
-	return filterFunction
-}
+		
+		if(doFilter){
+			this.#data.filtered.items = this.#data.raw.items.filter(filterFunctionCreator(ITEMS.NSFW))
+			this.#data.filtered.curses = this.#data.raw.curses.filter(filterFunctionCreator(CURSES.NSFW))
+		}
 
-// get ticket value, determine required filters, call rank filter, then return filtered results.
-function FilterTicketData(itemsData) {
-	var ticketValue = document.getElementById("ticketSelector").value;
-	var filter;
-	switch (ticketValue) {
-		case "0":
-		default:
-			return itemsData;
-			break;
-		case "1":
-			filter = ["F", "E", "D"];
-			break;
-		case "2":
-			filter = ["E", "D", "C"];
-			break;
-		case "3":
-			filter = ["D", "C", "B"];
-			break;
-		case "4":
-			filter = ["C", "B", "A"];
-			break;
-		case "5":
-			filter = ["B", "A", "S"];
-			break;
-		case "6":
-			filter = ["A", "S", "SS"];
-			break;
-		case "7":
-			filter = ["S", "SS", "SSS"];
-			break;
-		case "8":
-			filter = ["SS", "SSS", "EX"];
-			break;
+		this.updateRollData()
+	}
+	/**
+	 * get a constant reference to data.filtered.items, mutate acordingly
+	 * and assign to data.roll.items
+	 */
+	updateItemRollData() {
+		const rollItemsData = this.#data.filtered.items
+
+		rollItemsData = this.filterItemByRank(rollItemsData);
+		rollItemsData = this.filterItemByCategory(rollItemsData);
+		this.#data.roll.items = rollItemsData
 	}
 
-
-
-	return itemsData.filter(valueFilter(filter, ITEMS.RANK));
-}
-// determine categories to filter by, (if any if all is checked), then call filter with valueFilter
-function filterItemByCategory(itemsData) {
-	var itemsCategoriesFilters = document.querySelectorAll("#itemsCategoryFilter input");
-	var filter = []
-
-	if (itemsCategoriesFilters[0].checked) return itemsData;
-
-	for (var category of itemsCategoriesFilters) {
-		if (category.checked) filter.push(category.value);
+	/**
+	 * currently, just sets roll.curses to filtered.curses
+	 * method and additional variable exists for future proofing.
+	 */
+	#updateCurseRollData() {
+		this.#data.roll.curses = this.#data.filtered.curses
 	}
 
-	return itemsData.filter(valueFilter(filter, ITEMS.CATEGORY))
-}
-// shabby but technically works.... for now.
-function searchFor(string) {
-	var regex = new RegExp(string, "i");
-	var searchResults = itemsData.filter(function (item) {
-		return item[ITEMS.NAME].concat(item[ITEMS.SERIES], item[ITEMS.DESCRIPTION]).search(regex) != -1;
-	});
-
-	var tbody = document.querySelector("#searchTable > tbody");
-	tbody.innerHTML
-		= ""; //wipe before replace
-
-	for (var i = 0; i < searchResults.length; i++) {
-		if(!searchResults[i]) break;
-		var element = searchResults[i];
-		var newRow = document.createElement("tr");
-		newRow.innerHTML = "<td>" + element[0] + "</td><td>" + element[1] + "</td><td>" + element[2] + "</td><td>" + element[3] + "</td><td>" + element[4] + "</td><td>" + element[5] + "</td><td>" + element[6] + "</td><td>" + element[7] + "</td><td>" + element[8] + "</td><td>" + element[9] + "</td><td>" + element[10] + "</td><td>" + element[11] + "</td><td>" + element[12] + "</td><td>" + element[13] + "</td><td>" + element[14] + "</td><td>" + element[15] + "</td><td>" + element[16] + "</td><td>" + element[17] + "</td><td>" + element[18] + "</td><td>" + element[19] + "</td><td>" + element[20] + "</td>";
-		tbody.append(newRow);
+	/**
+	 * update #data.roll
+	 */
+	updateRollData() {
+		this.#filterItemByRank
+		this.updateCurseRollData()
 	}
-}
 
-export {
-	contentFilterChange,
-	updateContentFilter,
-	updateItemFilterData,
-	updateCurseFilterData,
-	updateFilterData,
-	valueFilter,
-	textValueFilter,
-	FilterTicketData,
-	filterItemByCategory,
-	searchFor,
-};
+	/**
+	 * filter by exact value
+	 * for filter by contains, use textFilter
+	 * 
+	 * items and curses are essentially, arrays, of a bunch of data
+	 * when trying to filter by category or name or rank, you call this function
+	 * simply provide the index of what you're filtering ie; ITEMS.RANK
+	 * and filterArray to check the value, of index against.
+	 * 
+	 * @param {string[]} filterArray array of things to match to
+	 * @param {int} index what index of the item/curse to check against
+	 * @returns function to use in array.filter()
+	 */
+	static exactValueFilter(filterArray, index) {
+		var filterFunction = function (value) {
+			const valuePart = value[index].toLowerCase();
+			for (filter of filterArray) {
+				if (valuePart === filter.toLowerCase()) return true
+			}
+			return false;
+		}
+		return filterFunction
+	}
+	
+	/**
+	 * filter by text value. text only needs to be part of, not exact match
+	 * for exact match use valueFilter
+	 * 
+	 * items and curses are essentially, arrays, of a bunch of data
+	 * when trying to filter by category or name or rank, you call this function
+	 * simply provide the index of what you're filtering ie; ITEMS.NAME
+	 * and filterArray to check the value, of index against.
+	 * 
+	 * @param {string[]} filterArray array of things to match to
+	 * @param {int} index what index of the item/curse to check against
+	 * @returns function to use in array.filter()
+	 */
+	static textValueFilter(filterArray, index) {
+		var filterFunctionCreator = function (value) {
+			const valuePart = value[index].toLowerCase();
+			for (filter of filterArray) {
+				if (valuePart.includes(filter.toLowerCase())) return true
+			}
+			return false;
+		}
+		return filterFunctionCreator
+	}
+
+	/**
+	 * filter items by rank determined in #filters.ranks
+	 * @param {string[]} itemsData array of items
+	 * @returns filtered items array
+	 */
+	#filterItemByRank(itemsData) {
+		return itemsData.filter(valueFilter(this.#filters.ranks, ITEMS.RANK));
+	}
+	// determine categories to filter by, (if any if all is checked), then call filter with valueFilter
+	filterItemByCategory(itemsData) {
+		var itemsCategoriesFilters = document.querySelectorAll("#itemsCategoryFilter input");
+		var filter = []
+
+		if (itemsCategoriesFilters[0].checked) return itemsData;
+
+		for (var category of itemsCategoriesFilters) {
+			if (category.checked) filter.push(category.value);
+		}
+
+		return itemsData.filter(valueFilter(filter, ITEMS.CATEGORY))
+	}
+	// shabby but technically works.... for now.
+	searchFor(string) {
+		var regex = new RegExp(string, "i");
+		var searchResults = itemsData.filter(function (item) {
+			return item[ITEMS.NAME].concat(item[ITEMS.SERIES], item[ITEMS.DESCRIPTION]).search(regex) != -1;
+		});
+
+		var tbody = document.querySelector("#searchTable > tbody");
+		tbody.innerHTML
+			= ""; //wipe before replace
+
+		for (var i = 0; i < searchResults.length; i++) {
+			if (!searchResults[i]) break;
+			var element = searchResults[i];
+			var newRow = document.createElement("tr");
+			newRow.innerHTML = "<td>" + element[0] + "</td><td>" + element[1] + "</td><td>" + element[2] + "</td><td>" + element[3] + "</td><td>" + element[4] + "</td><td>" + element[5] + "</td><td>" + element[6] + "</td><td>" + element[7] + "</td><td>" + element[8] + "</td><td>" + element[9] + "</td><td>" + element[10] + "</td><td>" + element[11] + "</td><td>" + element[12] + "</td><td>" + element[13] + "</td><td>" + element[14] + "</td><td>" + element[15] + "</td><td>" + element[16] + "</td><td>" + element[17] + "</td><td>" + element[18] + "</td><td>" + element[19] + "</td><td>" + element[20] + "</td>";
+			tbody.append(newRow);
+		}
+	}
+
+
+}
